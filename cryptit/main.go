9dc479337444bcdf7b00b3c91ce8c0137e010173
pradumkumar6 <pradumjaiswar790@gmail.com>
2024-07-26 08:34:20 +0530

    First Commit

---
+++ cryptit/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pradumkumar6/cryptit/cryptit/encrypt"
+)
+
+func main() {
+	fmt.Println(encrypt.Nimbus("Kodecloud"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pradumkumar6/cryptit
+
+go 1.22.4
